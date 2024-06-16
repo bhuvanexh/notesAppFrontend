2a3dee7d80d6a0a03501ada9fb4375131b03b25d
Bhuvanesh <bhuvaneshchoudhary9521@gmail.com>
2024-06-16 23:30:11 +0530

    hmm

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,8 +1,8 @@
 export default function Loading() {
     return (
         <>
-            <div>
-                <h1>Loading...</h1>
+            <div className="flex items-center justify-center h-screen">
+                <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-gray-900"></div>
             </div>
         </>
     )
--- src/Components/ToolsComponent/TodoComponents/TodoWrapper.jsx
+++ src/Components/ToolsComponent/TodoComponents/TodoWrapper.jsx
@@ -43,16 +43,18 @@ export const TodoWrapper = () => {
     return (
         <>
             <div className='bg-black h-full flex justify-center items-start pt-20'>
-                <div className='flex w-[95%] sm:w-[50%] sm:min-w-[550px] md:min-w-[650px] flex-col text-white border bg-[#34082dce] border-slate-500 p-4 items-center rounded-lg gap-3'>
+                <div className='flex relative w-[95%] sm:w-[50%] sm:min-w-[550px] md:min-w-[650px] flex-col text-white border bg-[#34082dce] border-slate-500 p-4 items-center rounded-lg gap-3'>
                     <h1 className='text-white text-xl sm:text-2xl mb-3'>Get Things Done!</h1>
                     <TodoForm addTodo={addTodo} />
-                    {todos.map((todo, index) => (
-                        todo.isEditing ? (
-                            <EditTodoForm key={index} editTodo={editTask} task={todo} />
-                        ) : (
-                            <Todo task={todo} key={index} toggleComplete={toggleComplete} deleteTodo={deleteTodo} editTodo={editTodo} />
-                        )
-                    ))}
+                    <div className='flex flex-col gap-3 h-[450px] overflow-y-scroll px-3 py-2'>
+                        {todos.map((todo, index) => (
+                            todo.isEditing ? (
+                                <EditTodoForm key={index} editTodo={editTask} task={todo} />
+                            ) : (
+                                <Todo task={todo} key={index} toggleComplete={toggleComplete} deleteTodo={deleteTodo} editTodo={editTodo} />
+                            )
+                        ))}
+                    </div>
                 </div>
             </div>
         </>
